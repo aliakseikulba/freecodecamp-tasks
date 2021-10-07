ddff96744ea7ee2952f731da0012fa4f69ba17c0
aliakseikulba <kulba.av@gmail.com>
2021-10-07 14:34:06 +0300

    characters classes

---
+++ regexp/CharacterClasses.js
@@ -0,0 +1,13 @@
+// let bigStr = "big";
+// let bagStr = "bag";
+// let bugStr = "bug";
+// let bogStr = "bog";
+// let bgRegex = /b[aiu]g/;
+// bigStr.match(bgRegex);
+// console.log(bagStr.match(bgRegex));;
+// bugStr.match(bgRegex);
+// bogStr.match(bgRegex);
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi;
+let result = quoteSample.match(vowelRegex);
