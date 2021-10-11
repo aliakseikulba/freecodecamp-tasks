135f022411f3ebde7049a81f0422d5e96ddefea6
aliakseikulba <kulba.av@gmail.com>
2021-10-11 16:24:08 +0300

    multi-dimentional array (5)

---
+++ basicDataStructures/MultiDementional.js
@@ -0,0 +1,15 @@
+let myNestedArray = [
+  // Only change code below this line
+  [
+    ['loop', 'shift', 6, 7, 1000, 'method', 'deep'],
+    [
+      ['concat', false, true, 'spread', 'array', 'deeper'],
+      [
+        ['mutate', 1327.98, 'splice', 'slice', 'push', 'deepest']
+      ]
+    ]
+  ],
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+  // Only change code above this line
+];
